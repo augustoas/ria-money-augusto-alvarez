a4f80a11a2b235861e575795656707ec40f59210
Augusto Alvarez Smith <augustoalvarezsmith@Augustos-MacBook-Pro.local>
2024-08-15 10:40:36 -0400

    readme

--- ria/src/components/HourlyWeatherCard.vue
+++ ria/src/components/HourlyWeatherCard.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, computed } from "vue";
+import { defineProps } from "vue";
 import { formatTemperature, formatHour } from "@/utils/utils";
 import type { HourlyWeather } from "../types/types";
 
